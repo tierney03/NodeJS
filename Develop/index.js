14a5205cbb6e304286691c39b09b899ea1530643
tierney03 <tierney_03@mac.com>
2022-10-03 10:01:42 -0500

    Adding array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,57 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install the project?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license.',
+        choices: ['Apache 2.0', 'MIT', 'ISC', 'none']
+    },
+    {
+        type: 'input',
+        name: 'git',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
